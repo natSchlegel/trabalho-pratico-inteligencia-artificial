c93bb61f9e2f46f35f24059371514559cc07a787
natSchlegel <46815651+natSchlegel@users.noreply.github.com>
2020-12-05 21:07:05 -0300

    Atualizado

--- busca-a-estrela/main.cpp
+++ busca-a-estrela/main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <list>
 
 using namespace std;
 
+struct Tabuleiro {
+int mat[3][3] = {{1,2,3},{4,5,6},{7,0,8}};
+int posx = 2;
+int posy = 1;
+};
+
+int obj[3][3] = {{1,2,3},{4,5,6},{7,8,0}};
+
+void reconhecerCaminho(Tabuleiro){
+
+
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+
     return 0;
 }
--- busca-largura/main.cpp
+++ busca-largura/main.cpp
@@ -1,20 +1,24 @@
 #include <queue>
 #include <iostream>
+#include <vector>
+
 using namespace std;
+
 int obj[3][3] = {{1,2,3},{4,5,6},{7,8,0}};
-struct Tabuleiro
-{
-    int mat[3][3] = {{1,2,3},{4,5,6},{7,0,8}};
-    int posx = 2;
-    int posy = 1;
+
+struct Tabuleiro{
+    int mat[3][3] = {{1,2,3},{4,8,5},{7,0,6}};
+    int posx = 2; // linha
+    int posy = 1; // coluna
+    int pai = 0;
     bool explorado = false;
 };
-bool validar(int x, int y)
-{
+
+bool validar(int x, int y){
     return !(x < 0 || x > 2 || y < 0 || y > 2);
 }
-bool Ehobjetivo(Tabuleiro tabuleiro)
-{
+
+bool finalizar(Tabuleiro tabuleiro){
     for(int i=0; i<3; i++){
         for(int j=0; j<3; j++){
             if(tabuleiro.mat[i][j] != obj[i][j])
@@ -23,8 +27,8 @@ bool Ehobjetivo(Tabuleiro tabuleiro)
     }
     return true;
 }
-void imprimeTabuleiro(Tabuleiro tabuleiro)
-{
+
+void imprimeTabuleiro(Tabuleiro tabuleiro){
     for(int i=0; i<3; i++){
         for(int j=0; j<3; j++){
             cout << tabuleiro.mat[i][j] << " | ";
@@ -34,39 +38,65 @@ void imprimeTabuleiro(Tabuleiro tabuleiro)
     cout << endl;
     return;
 }
-int main()
-{
-    Tabuleiro tabuleiro;
-    queue<Tabuleiro> fazer;
-    queue<Tabuleiro> feito;
-    int posicaox[4] = {1,-1,0,0};
-    int posicaoy[4] = {0,0,1,-1};
-    cout << "Estado Inicial:";
-    cout << endl;
-    imprimeTabuleiro(tabuleiro);
-    fazer.push(tabuleiro);
-    while(fazer.size()>0){
-        Tabuleiro tabuleiro = fazer.front();
-        fazer.pop();
-        cout << "Estado Atual: " << endl;
+
+void buscar(Tabuleiro tabuleiro){
+
+queue<Tabuleiro> lista;
+vector<Tabuleiro> caminho;
+
+int posicaox[4] = {1,-1,0,0};
+int posicaoy[4] = {0,0,1,-1};
+
+lista.push(tabuleiro);
+
+while(lista.size()>0){
+
+    Tabuleiro tabuleiro = lista.front();
+
+    caminho.push_back(tabuleiro);
+    int pos = caminho.size()-1;
+
+    lista.pop();
+
+    if(finalizar(tabuleiro)){
+
+        cout << "Caminho:" << endl;
+        cout << " " << endl;
+        Tabuleiro novoTabuleiro;
         imprimeTabuleiro(tabuleiro);
-        if(Ehobjetivo(tabuleiro))
-            return 0;
-        cout << "Sucessores: " << endl;
-        for (int i = 0; i < 4; i++)
-        {
-            int pox = tabuleiro.posx + posicaox[i];
-            int poy = tabuleiro.posy + posicaoy[i];
-            if (validar(pox,poy))
-            {
-                Tabuleiro sucessor = tabuleiro; //referencia ou valor
-                swap(sucessor.mat[sucessor.posx][sucessor.posy],sucessor.mat[pox][poy]);
+        int aux = tabuleiro.pai;
+
+        for(int i = 3; i > 0; i--) { /*o for eh finito e preciso pois ainda nao consegui descobrir como escrever de forma recursiva*/
+        novoTabuleiro = caminho[aux];
+        aux = novoTabuleiro.pai;
+        imprimeTabuleiro(novoTabuleiro);
+        }
+
+        return;
+    }
+
+    for (int i = 0; i < 4; i++){
+
+        int pox = tabuleiro.posx + posicaox[i];
+        int poy = tabuleiro.posy + posicaoy[i];
+
+        if (validar(pox,poy)){
+
+        Tabuleiro sucessor = tabuleiro;
+        swap(sucessor.mat[sucessor.posx][sucessor.posy],sucessor.mat[pox][poy]);
+
                 sucessor.posx = pox;
                 sucessor.posy = poy;
-                fazer.push(sucessor);
-                imprimeTabuleiro(sucessor);
+                sucessor.pai = pos;
+
+                lista.push(sucessor);
             }
         }
     }
+}
+
+int main(){
+    Tabuleiro tabuleiro;
+    buscar(tabuleiro);
     return 0;
 }

7a4a30a6831c827c8b00ab0580d4e279773bf4ce
natSchlegel <46815651+natSchlegel@users.noreply.github.com>
2020-12-09 20:53:14 -0300

    Atualizado

--- busca-gulosa/main.cpp
+++ busca-gulosa/main.cpp
@@ -3,16 +3,16 @@
 #include <stack>
 #include <list>
 #include <queue>
+#include <vector>
 
 using namespace std;
 
 struct Tabuleiro{
-list<Tabuleiro> sucessores;
-list<Tabuleiro> posSuc;
 int mat[3][3] = {{1,2,3},{4,5,6},{7,0,8}};
 int posx = 2;
 int posy = 1;
 int contagem = 0;
+list<Tabuleiro> sucessores;
 };
 
 int obj[3][3] = {{1,2,3},{4,5,6},{7,8,0}};
@@ -20,12 +20,13 @@ int obj[3][3] = {{1,2,3},{4,5,6},{7,8,0}};
 Tabuleiro tabuleiro;
 
 queue<Tabuleiro> queueTabuleiro;
+vector<Tabuleiro> caminho;
 
 bool validar(int x, int y){
     return !(x < 0 || x > 2 || y < 0 || y > 2);
 }
 
-bool conferir(Tabuleiro tabuleiro){
+bool finalizar(Tabuleiro tabuleiro){
     for(int i=0; i<3; i++){
         for(int j=0; j<3; j++){
             if(tabuleiro.mat[i][j] != obj[i][j])
@@ -35,42 +36,53 @@ bool conferir(Tabuleiro tabuleiro){
     return true;
 }
 
-void compararPecas(Tabuleiro tabuleiro){
-    for(int i=0; i<3; i++){
+struct comparar{
+    bool operator()(Tabuleiro const& a, Tabuleiro const& b)
+    {
+        return a.contagem < b.contagem;
+    }
+};
+
+void buscar(Tabuleiro tabuleiro){
+    cout<<"Teste 1"<<endl;
+    if(finalizar(tabuleiro)){
+        cout<<"sucesso"<<endl;
+    }
+
+    priority_queue <Tabuleiro, vector<Tabuleiro>, comparar> filaPrioridade;
+    cout<<"Teste 2"<<endl;
+    int posicaox[4] = {1,-1,0,0};
+    int posicaoy[4] = {0,0,1,-1};
+
+    for (int i = 0; i < 4; i++){
+        int pox = tabuleiro.posx + posicaox[i];
+        int poy = tabuleiro.posy + posicaoy[i];
+         cout<<"Teste 3"<<endl;
+        if (validar(pox,poy)){
+                cout<<"Teste 4"<<endl;
+            Tabuleiro sucessor = tabuleiro;
+            swap(sucessor.mat[sucessor.posx][sucessor.posy],sucessor.mat[pox][poy]);
+            sucessor.posx = pox;
+            sucessor.posy = poy;
+            filaPrioridade.push(sucessor);
+        }
+        cout<<"Teste 5"<<endl;
+    }
+    cout<<"Teste 6"<<endl;
+        Tabuleiro aux;
+        aux = filaPrioridade.top();
+        caminho.push_back(aux);
+        cout<<"Tamanho" << caminho.size()<<endl;
+            for(int i=0; i<3; i++){
         for(int j=0; j<3; j++){
-            if(tabuleiro.mat[i][j] != obj[i][j]){
-                tabuleiro.contagem++;
-            }
+            cout << tabuleiro.mat[i][j] << " | ";
         }
+        cout << endl;
     }
+    cout << endl;
+        buscar(aux);
 }
 
-void compSuc(Tabuleiro sucessor, Tabuleiro tabuleiro){
-
-    /*comparar os números de tabuleiro.contagem e retornar o menor*/
-
-}
-
-void movimentarTabuleiro(Tabuleiro tabuleiro){
-
-int posicaox[4] = {1,-1,0,0};
-int posicaoy[4] = {0,0,1,-1};
-
-queueTabuleiro.push(tabuleiro);
-
-for (int i = 0; i < 4; i++){
-int pox = tabuleiro.posx + posicaox[i];
-int poy = tabuleiro.posy + posicaoy[i];
-if (validar(pox,poy)){
-        Tabuleiro sucessor = tabuleiro;
-        swap(sucessor.mat[sucessor.posx][sucessor.posy],sucessor.mat[pox][poy]);
-        sucessor.posx = pox;
-        sucessor.posy = poy;
-        compararPecas(sucessor);
-        (tabuleiro.posSuc).push_front(sucessor);
-        }
-    }
-}
 
 void imprimeTabuleiro(Tabuleiro tabuleiro){
     for(int i=0; i<3; i++){
@@ -83,18 +95,7 @@ void imprimeTabuleiro(Tabuleiro tabuleiro){
     return;
 }
 
-void buscar(Tabuleiro tabuleiro){
-    queueTabuleiro.push(tabuleiro);
-    while(queueTabuleiro.size()>0){
-        movimentarTabuleiro(tabuleiro);
-    }
-    return;
-}
-
 int main(){
-
-buscar(tabuleiro);
-
-/*erro: não é criado a fila queueTabuleiro */
-
+    buscar(tabuleiro);
+    //falta criar a lista de comparação para não dar loop infinito
 }
